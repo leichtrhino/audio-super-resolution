e04a5117e544c69de0aac0ec8aa5311763a8d390
leichtrhino <leichtrhino@outlook.jp>
2021-06-01 08:24:02 +0900

    Update model_settings.py

--- model_settings.py
+++ model_settings.py
@@ -8,8 +8,7 @@ def conv_kernel_size():
     return [3, 9, 27, 81]
 
 def conv_out_channels():
-    #return [16, 32, 64, 128]
-    return [8, 16, 32, 64]
+    return [64, 128, 256, 512]
 
 def out_conv_in_channel():
     return 32
@@ -21,16 +20,12 @@ def sample_sr():
     return 44100
 
 def sample_duration():
-    return 3.0
+    return sample_segment_length() / sample_sr()
 
 def sample_segment_length():
-    orig_segments = math.ceil(sample_sr() * sample_duration())
+    orig_segments = math.ceil(sample_sr() * 0.05)
     segment_factor = supersampling_rate()**len(conv_out_channels())
     return math.ceil(orig_segments / segment_factor) * segment_factor
 
 def out_linear_features():
-    return math.ceil(math.sqrt(max(
-        conv_out_channels()[0],
-        sample_segment_length()
-        // (supersampling_rate()**len(conv_out_channels()))
-    )))
+    return 1024
