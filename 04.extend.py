58095cc7ef00f5a58a0e1c70f91f28da0404ad80
leichtrhino <leichtrhino@outlook.jp>
2021-06-08 23:35:43 +0900

    Update 04.extend.py to add minibatch

--- 04.extend.py
+++ 04.extend.py
@@ -1,4 +1,5 @@
 #!/usr/env python
+import math
 import argparse
 import resampy
 import torch
@@ -14,6 +15,7 @@ def parse_args():
     parser.add_argument('output', type=str)
     parser.add_argument('--gpu', action='store_true')
     parser.add_argument('--checkpoint', type=str, default=None)
+    parser.add_argument('--batch-size', type=int, default=32)
 
     args = parser.parse_args()
     if args.gpu:
@@ -43,18 +45,44 @@ def main():
         orig_sr,
         model_settings.sample_sr() // model_settings.supersampling_rate(),
         axis=-1
-    )).to(args.device)
+    ))
+    orig_length = x_l.shape[-1]
+    sample_num = math.ceil(
+        x_l.shape[-1] / model_settings.sample_segment_length())
+
+    x_l = torch.cat((
+        x_l,
+        torch.zeros(
+            *x_l.shape[:-1],
+            model_settings.sample_segment_length() * sample_num - x_l.shape[-1]
+        )
+    ), dim=-1).reshape(
+        *x_l.shape[:-1], sample_num, model_settings.sample_segment_length()
+    )
 
     # build (and load) a model
     generator = build_generator()
     checkpoint = torch.load(args.checkpoint)
     generator.load_state_dict(checkpoint['generator'])
+    generator.eval()
     generator.to(args.device)
 
     # extend
-    generator.eval()
-    with torch.no_grad():
-        x_hat = generator(x_l).to('cpu')
+    out_tensors = []
+    for channel_i, channel in enumerate(x_l):
+        for batch_i in range(0, channel.shape[0], args.batch_size):
+            batch_end_i = min(batch_i + args.batch_size, channel.shape[0])
+            batch = channel[batch_i:batch_end_i]
+            with torch.no_grad():
+                x_hat = generator(batch.to(args.device)).to('cpu').flatten()
+
+            if len(out_tensors) == channel_i:
+                out_tensors.append(x_hat)
+            else:
+                out_tensors[channel_i] = torch.cat((
+                    out_tensors[channel_i], x_hat))
+
+    x_hat = torch.stack(out_tensors)[:, :orig_length]
 
     # save
     torchaudio.save(args.output, x_hat, sample_rate=model_settings.sample_sr())
